7d201f938aab90ba509ae2f291a179b55ee927d4
Tran Tu <tratu@cisco.com>
2017-10-21 15:57:32 -0700

    group speaker

--- react/src/Duration.js
+++ react/src/Duration.js
@@ -2,27 +2,57 @@ import React, { Component } from 'react';
 import Plotly from 'plotly.js/dist/plotly-cartesian';
 
 class Duration extends Component {
+  constructor(props) {
+    super(props);
+    this.speakerColor = {};
+    this.colorIndex = 0;
+  }
+
+  pickColor(speaker) {
+    let colors = [
+      '#1f77b4',  // muted blue
+      '#ff7f0e',  // safety orange
+      '#2ca02c',  // cooked asparagus green
+      '#d62728',  // brick red
+      '#9467bd',  // muted purple
+      '#8c564b',  // chestnut brown
+      '#e377c2',  // raspberry yogurt pink
+      '#7f7f7f',  // middle gray
+      '#bcbd22',  // curry yellow-green
+      '#17becf'   // blue-teal
+    ];
+    if (!this.speakerColor[speaker]) {
+      this.speakerColor[speaker] = colors[this.colorIndex];
+      this.colorIndex++;
+      if (this.colorIndex > colors.length) {
+        this.colorIndex = 0;
+      }
+    }
+  }
+
   componentWillReceiveProps(nextProps) {
     console.log('willreceive');
-
     let durationData = nextProps.durationData;
     let data = [];
     for(let i = 0; i < durationData.length; i++) {
-      // console.log('duration', durationData[i].end - durationData[i].start);
+      this.pickColor(durationData[i].speaker);
       data.push({
         x: [durationData[i].end_time - durationData[i].start_time], // value
         y: ['track'],
+        legendgroup: durationData[i].speaker,
         text: durationData[i].speaker,
         name: durationData[i].speaker, // category
         orientation: 'h', // horizontal
+        marker: {color: this.speakerColor[durationData[i].speaker]},
         // textposition: 'auto',
         // hoverinfo: 'none',
-        type: 'bar'  
+        type: 'bar'
       })
     }
 
     this.myPlot.data = data;
     Plotly.redraw(this.myPlot);
+    console.log(Plotly.d3.color());
   }
 
   componentWillUpdate() {
